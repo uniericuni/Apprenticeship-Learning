1b043581d71b168ff6309a672f217139750fee02
Uniericuni <uniericuni@gmail.com>
2016-12-04 17:28:07 -0500

    [Merge]GGG

--- car_simulator/trMain.py
+++ car_simulator/trMain.py
@@ -2,19 +2,29 @@ import pygame
 import game
 import time
 import scipy.io as sio
+from carAgents import *
 from config import *
+
+MAX_ITERATION = 10
 
 if __name__ == '__main__':
 
     # game manager init
-    gamemgr = game.gamemgr(state=100)
+    counter = 0
     features = []
+    gamemgr = game.gamemgr(state=100)
+    status = gamemgr.input()
+    state,legal_action = gamemgr.update()
+
+    # agent init
+    agent = CarAgent(w=np.zeros([15,1]))
+    agent.registerInitialState((state,legal_action))
 
     # main game loop
-    while gamemgr.state:
+    while gamemgr.state or counter>MAX_ITERATION:
 
         # action assignment
-        action = 4
+        action = agent.getAction((state,legal_action))
 
         # main game loop
         status = gamemgr.input(action)
@@ -28,6 +38,9 @@ if __name__ == '__main__':
             timestr = time.strftime('%y%m%d%H%M%S')
             sio.savemat('%s%s_record.mat'%(CAR_RECORD_PATH,timestr), {'features':features})
             features = []
+        
+    # counter
+    agent.final((state,legal_action))
 
     # feature export
     if len(features)>0:

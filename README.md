1f755a7469e97340649a5a95e75d0f262d18e826
Uniericuni <uniericuni@gmail.com>
2016-12-05 20:03:51 -0500

    [Trivial]

--- README.md
+++ README.md
@@ -7,14 +7,10 @@
 - LEFT: move left
 - ESC: quit game
 
-## TODO:
-- Modified style of lane
-- Turing head animation
-- Quantized lane-changing control
-- Sprite
-    - Car frequency
-    - Lane number
-    - Difficulty
+## Car Simulation
+### Commands
+- python main.py -m gamemode -i maxTrainginIteration
+    - gamemode: train|drive|auto
 
 ## References
 - [1] Abbeel, Pieter, and Andrew Y. Ng. "Apprenticeship learning via inverse reinforcement learning." Proceedings of the twenty-first international conference on Machine learning. ACM, 2004.
--- car_simulator/main.py
+++ car_simulator/main.py
@@ -1,26 +1,53 @@
-import pygame
-import game
-import time
+import pygame, sys, time
 import scipy.io as sio
+import game
+from parser import parser
 from config import *
 
-if __name__ == '__main__':
+# main
+def main(argv):
 
-    status = 1
-    gamemgr = game.gamemgr()
+    # command parser
+    gamemode,MAX_ITERATION = parser(argv)
+
+    # game manager init
     features = []
+    gamemgr = game.gamemgr(mode=gamemode)
+    mode = gamemgr.input()
+    feature,state,legal_action = gamemgr.update()
 
-    # main game loop
-    while status:
-        status = gamemgr.input()
-        feature = gamemgr.update()
-        gamemgr.render()
-        if feature:
-            features.append(feature)
-        elif feature==None and len(features)>0:
-            timestr = time.strftime('%y%m%d%H%M%S')
-            sio.savemat('%s%s_record.mat'%(CAR_RECORD_PATH,timestr), {'features':features})
-            features = []
+    # training mode
+    if gamemode==100:
+        agent = CarAgent(w=np.zeros([15,1]))
+        learn = CarLearning( agent, gamemgr, maxIter=MAX_ITERATION, numEstimating=10, numTraining=10)
+        learn.computeExpertExpectation()
+        learn.train()
+
+    # playing mode
+    else:
+        # main game loop
+        while gamemgr.mode:
+            mode = gamemgr.input()
+            feature,state,legal_action = gamemgr.update()
+            gamemgr.render()
+
+            # feature export
+            if gamemgr.record:
+                features.append(feature)
+            elif len(features)>0:
+                timestr = time.strftime('%y%m%d%H%M%S')
+                sio.savemat('%s%s_record.mat'%(CAR_RECORD_PATH,timestr), {'features':features})
+                features = []
+
+    # feature export
     if len(features)>0:
         timestr = time.strftime('%y%m%d%H%M%S')
         sio.savemat('%s%s_record.mat'%(CAR_RECORD_PATH,timestr), {'features':features})
+
+# main
+if __name__ == '__main__':
+    # import agent
+    sys.path.append(AGENT_PATH)
+    from carAgents import *
+    from carLearning import *
+    main(sys.argv[1:])

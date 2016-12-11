ec01c11fdcc1e4964a710567490eeb1032d36388
Kuan-Yu Chen <kyuchen@umich.edu>
2016-12-10 22:21:32 -0500

    Bad Bad Agent

--- agent/gridWorldLearning.py
+++ agent/gridWorldLearning.py
@@ -19,51 +19,89 @@ class gridWorldLearning(InverseLearning):
 	def runGame(self):
 		#startState = self.gamemgr.getStartState()
 		currentState = self.gamemgr.getStartState()
+		self.gamemgr.setCurrentState(currentState)
 		self.gamemgr.setNegativeScore(0)
 		cx, cy = self.gamemgr.int_to_point(currentState)
 
 		self.agent.registerInitialState(currentState)
 		count = 0
-		print(self.gamemgr.convertToMatrix(self.gamemgr.ground_r))
-		print('Goal:', self.gamemgr.int_to_point(self.gamemgr.getPostiveRewardState()))
+		#print(self.gamemgr.convertToMatrix(self.gamemgr.ground_r))
+		#print('Goal:', self.gamemgr.int_to_point(self.gamemgr.getPostiveRewardState()))
+		if self.agent.isInTesting():
+			print(self.gamemgr.convertToMatrix(self.gamemgr.ground_r))
+			print('Goal:', self.gamemgr.int_to_point(self.gamemgr.getPostiveRewardState()))
+			while True:
+				currentState = self.gamemgr.getCurrentState()
+				cx, cy = self.gamemgr.int_to_point(currentState)
+				print('=========================')		
+				print('Current state: ', (cx, cy))
+				p = np.random.uniform(0, 1)
 
-		while True:
-			cx, cy = self.gamemgr.int_to_point(self.gamemgr.getCurrentState())
-			print('=========================')
-			#print('Current state: ', (cx, cy))
-			
-			p = np.random.uniform(0, 1)
+				if  p > self.gamemgr.wind:	
+					action = self.agent.getAction(currentState) # Specific action
+					print('Specific action:', action)
+					ax, ay = self.gamemgr.getAction(action)     # Action to tuple
 
-			if  p > self.gamemgr.wind:		
+				else:
+					action = np.random.randint(0, 4)
+					print('Random action', action)
+					ax, ay = self.gamemgr.getAction(action)
 
-				action = self.agent.getAction(currentState) # Specific action
-				print('Specific action:', action)
-				ax, ay = self.gamemgr.getAction(action)     # Action to tuple
+				
+				print('Next State:', (cx + ax, cy + ay))
+				if cx + ax < 0 or cx + ax >= self.gamemgr.grid_size or cy + ay < 0 or cy + ay >= self.gamemgr.grid_size:
+					print('Out of Grid!')
+					self.gamemgr.increaseNegativeScore(-1)
+					continue
 
-			else:
-				action = np.random.randint(0, 4)
-				print('Random action', action)
-				ax, ay = self.gamemgr.getAction(action)
+				next_state = self.gamemgr.point_to_int((cx + ax, cy + ay))
+				if self.gamemgr.ground_r[next_state] == -1:
+					self.gamemgr.increaseNegativeScore(-1)
+					print('Step on negative!!')
+				self.gamemgr.setCurrentState(next_state)
+				#new_state = self.gamemgr.getCurrentState()
+				if next_state == self.gamemgr.getPostiveRewardState():
+					print('Good!')
+					break
+				count += 1
+		else:
+			while True:
+				cx, cy = self.gamemgr.int_to_point(self.gamemgr.getCurrentState())
+				#print('=========================')		
+				#print('Current state: ', (cx, cy))
+				
+				p = np.random.uniform(0, 1)
 
-			
-			print('Next State:', (cx + ax, cy + ay))
-			if cx + ax < 0 or cx + ax >= self.gamemgr.grid_size or cy + ay < 0 or cy + ay >= self.gamemgr.grid_size:
-				print('Out of Grid!')
-				self.gamemgr.increaseNegativeScore(-1)
-				continue
+				if  p > self.gamemgr.wind:		
 
-			next_state = self.gamemgr.point_to_int((cx + ax, cy + ay))
-			if self.gamemgr.ground_r[next_state] == -1:
-				self.gamemgr.increaseNegativeScore(-1)
-				print('Step on negative!!')
-			self.gamemgr.setCurrentState(next_state)
-			#new_state = self.gamemgr.getCurrentState()
-			if next_state == self.gamemgr.getPostiveRewardState():
-				print('Good!')
-				break
-			count += 1
+					action = self.agent.getAction(currentState) # Specific action
+					#print('Specific action:', action)
+					ax, ay = self.gamemgr.getAction(action)     # Action to tuple
 
-		print('Negative Score: ', self.gamemgr.getNegativeScore())
+				else:
+					action = np.random.randint(0, 4)
+					#print('Random action', action)
+					ax, ay = self.gamemgr.getAction(action)
+
+				
+				#print('Next State:', (cx + ax, cy + ay))
+				if cx + ax < 0 or cx + ax >= self.gamemgr.grid_size or cy + ay < 0 or cy + ay >= self.gamemgr.grid_size:
+					#print('Out of Grid!')
+					self.gamemgr.increaseNegativeScore(-1)
+					continue
+
+				next_state = self.gamemgr.point_to_int((cx + ax, cy + ay))
+				if self.gamemgr.ground_r[next_state] == -1:
+					self.gamemgr.increaseNegativeScore(-1)
+					#print('Step on negative!!')
+				self.gamemgr.setCurrentState(next_state)
+				#new_state = self.gamemgr.getCurrentState()
+				if next_state == self.gamemgr.getPostiveRewardState():
+					#print('Good!')
+					break
+				count += 1
+
+		#print('Negative Score: ', self.gamemgr.getNegativeScore())
 
 if __name__ == "__main__":
 	from gridWorldAgent import *
@@ -81,6 +119,10 @@ if __name__ == "__main__":
 	learn.computeExpertExpectation()
 
 	learn.train()
+	learn.test()
+	learn.test()
+	learn.test()
+	learn.test()
 
 
 

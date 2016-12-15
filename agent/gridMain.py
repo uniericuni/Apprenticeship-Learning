4ba7644c80ba05332be602648b9d8eab31cfeeb9
Kuan-Yu Chen <kyuchen@umich.edu>
2016-12-15 14:07:05 -0500

    Experiment

--- agent/gridMain.py
+++ agent/gridMain.py
@@ -3,20 +3,30 @@ from gridWorldLearning import *
 import sys
 import numpy as np
 
+
+
+
 if __name__ == "__main__":
     from gridWorldAgent import *
     sys.path.append('../gridworld/')
     from GridWorld import *
-
-    DaPingTai = DaPingTai(4, 0.3, 1)
+    
+    DaPingTai = DaPingTai(5, 0.3, 1)
     goalstate = DaPingTai.getPostiveRewardState()
-    w = np.zeros((16, 1))
+    w = np.zeros((25, 1))
+    # w = np.array([0, 0, -1, 1, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, 0, 0]).reshape(16, 1)
     #w[goalstate] = 1
     agent = gridWorldAgent(w, DaPingTai)
 
     learn = gridWorldLearning(agent, DaPingTai)
 
-    print(learn.gamemgr.convertToMatrix(learn.gamemgr.ground_r)), goalstate
     learn.computeExpertExpectation()
+    # learn.updateAgent()
+    
     learn.train()
+    
+    learn.test()
+
+    learn.test()
+    learn.test()
     learn.test()
